748371cc8ff347bef66d4ca6514297608be15c4e
camila <camiladomato@gmail.com>
2021-05-21 11:40:48 -0300

    modificando carpetas

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Cities from './Cities';
-import Home from './Home'
-import SignIn from './SignIn'
-import SignUp from './SignUp';
-import Itineraries from './Itineraries'
-import Activities from './Activities';
+import Cities from './screens/Cities';
+import Home from './screens/Home'
+import SignIn from './screens/SignIn'
+import SignUp from './screens/SignUp';
+import Itineraries from './screens/Itineraries'
+import Activities from './screens/Activities';
 
 export default function App() {
   return (
